7b26b7e39a13dbf0f91228fe3ae1d4e8447a558f
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2022-08-08 12:41:57 +0530

    Update Sno_26_minimum_Size_Subarray_Sum.java

--- Arrays Questions/src/Medium/Sno_26_minimum_Size_Subarray_Sum.java
+++ Arrays Questions/src/Medium/Sno_26_minimum_Size_Subarray_Sum.java
@@ -2,27 +2,34 @@ package Medium;
 
 public class Sno_26_minimum_Size_Subarray_Sum {
 
-    public static int minSubArray(int[] nums, int target){
+    public static int minSubArray(int[] nums, int k){
         int i = 0, j = 0;
         int sum = 0;
         int result = Integer.MAX_VALUE;
         int n = nums.length;
-        int k = target;
         while(j < n){
             sum += nums[j];
 
             if(sum < k){
                 j++;
             } else if(sum >= k){
+                // save the minimum of current window ( i.e j - i + 1 ) and result till now.
+                // we are doing this b/c question ask from the minimum subarray with sum >= k
                 result = Math.min((j - i + 1),result);
 
+                // reduce the sum till sum becomes < k b/c
+                // to move sliding window forward , we have to make sum < k ( think why ? )
                 while(sum > k){
                     sum -= nums[i];
                     i++;
+
+                    // check if sum is still >= k , than
+                    // save the minimum of current window ( i.e j - i + 1 ) and result till now. ( think why ? )
                     if(sum >= k){
                         result = Math.min((j - i + 1), result);
                     }
                 }
+                // increase j
                 j++;
             }
         }
