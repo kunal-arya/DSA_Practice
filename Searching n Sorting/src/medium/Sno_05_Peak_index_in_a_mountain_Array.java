a10a20ccfbc9b1e19901cd770ee34553e8f9df6a
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2022-08-24 13:54:43 +0530

    Create Sno_05_Peak_index_in_a_mountain_Array.java

---
+++ Searching n Sorting/src/medium/Sno_05_Peak_index_in_a_mountain_Array.java
@@ -0,0 +1,54 @@
+package medium;
+
+public class Sno_05_Peak_index_in_a_mountain_Array {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5,7,6,3,2,0};
+
+        // Brute force - O(n)
+        // use linear search
+        int brute = linearSearch(arr);
+        System.out.println(brute);
+
+        // optimised Approach - O(logN)
+        // use binary search
+        int ans = binarySearch(arr);
+        System.out.println(ans);
+    }
+
+    static int linearSearch(int[] arr) {
+        int max = 0;
+        int maxNum = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(maxNum < arr[i]) {
+                maxNum = arr[i];
+                max = i;
+            }
+        }
+        return max;
+    }
+
+
+    static int binarySearch(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid+1]) {
+                // you are in dec part of array
+                // this may be the ans, but look at left
+                // this is why end != mid - 1
+                end = mid;
+            } else {
+                // you are in asc part of array
+                start = mid + 1; // because we know that mid+1 element > mid element
+            }
+        }
+        // in the end, start == end and pointing to the largest number because of the 2 checks above
+        // start and end are always trying to find max element in the above 2 checks
+        // hence, when they are pointing to just one element, that is the max one because that is what the checks say
+        // more elaboration: at every point of time for start and end, they have the best possible answer till that time
+        // and if we are saying that only one item is remaining, hence cuz of above line that is the best possible ans
+        return start; // or return end as both are =
+    }
+}
