3951c49412c3931b6f3a85ecac29e0544c09f3f4
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2022-10-16 14:53:23 +0530

    Circular linked list

--- LinkedList/src/main.java
+++ LinkedList/src/main.java
@@ -2,19 +2,45 @@
 
 public class main {
     public static void main(String[] args) {
-        LL list = new LL();
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(8);
-        list.insertFirst(17);
-        list.insertLast(18);
-        list.insert(100,4);
+        // Single LinkedList
+//        LL list = new LL();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertLast(18);
+//        list.insert(100,4);
+//        list.display();
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        System.out.println(list.delete(2));
+//        list.display();
+
+        // Double LinkedList
+//        DLL list = new DLL();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertFirst(18);
+//        list.insertLast(20);
+//        list.insert(34,2);
+//        list.insertAfter(2,22);
+//        list.display();
+
+        // Circular Linkedlist
+        CLL list = new CLL();
+        list.insert(4);
+        list.insert(3);
+        list.insert(5);
+        list.insert(10);
         list.display();
-        System.out.println(list.deleteFirst());
+        list.delete(4);
         list.display();
-        System.out.println(list.deleteLast());
-        list.display();
-        System.out.println(list.delete(2));
+        list.delete(10);
+        list.insert(15);
         list.display();
     }
 }
